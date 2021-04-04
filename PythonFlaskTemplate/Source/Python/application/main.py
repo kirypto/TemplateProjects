9c1bf6c68266ddf0b72b22562bda35bbe0f0a495
Garrett Hansen <kirypto@gmail.com>
2021-04-04 13:06:13 -0600

    Add cards property to main application

--- PythonFlaskTemplate/Source/Python/application/main.py
+++ PythonFlaskTemplate/Source/Python/application/main.py
@@ -1,4 +1,13 @@
+from typing import Set
+
+from domain.cards import Card
+
+
 class PythonFlaskTemplateApp:
+    _name: str
+    _description: str
+    _cards: Set[Card]
+
     @property
     def name(self) -> str:
         return self._name
@@ -7,6 +16,17 @@ class PythonFlaskTemplateApp:
     def description(self) -> str:
         return self._description
 
+    @property
+    def cards(self) -> Set[Card]:
+        return self._cards
+
     def __init__(self, *, name: str, description: str) -> None:
         self._name = name
         self._description = description
+        self._cards = {
+            Card(suite="hearts", value="10"),
+            Card(suite="hearts", value="jack"),
+            Card(suite="hearts", value="queen"),
+            Card(suite="hearts", value="king"),
+            Card(suite="hearts", value="ace"),
+        }
